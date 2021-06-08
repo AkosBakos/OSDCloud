7ffb1dd0cae71d811fd9e4cfcd80944aa430050b
AkosBakos <44706641+AkosBakos@users.noreply.github.com>
2021-06-08 21:23:36 +0200

    Update and rename Win10-Enterprise-21H1-EN-US.ps1 to Win10-Enterprise-21H1-EN-US_HP.ps1

--- Win10-Enterprise-21H1-EN-US_HP.ps1
+++ Win10-Enterprise-21H1-EN-US_HP.ps1
@@ -16,7 +16,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "21H1" -OSEdition Enterprise -ZTI
+Start-OSDCloud -Manufacturer HP -Product 8275 -OSLanguage en-us -OSBuild "21H1" -OSEdition Enterprise -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
