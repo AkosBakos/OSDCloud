3df918fcaeee5d57a28734043c973eee9bcba8c7
AkosBakos <44706641+AkosBakos@users.noreply.github.com>
2021-08-01 14:02:42 +0200

    Update EDUBS_PoC.ps1

--- EDUBS_PoC.ps1
+++ EDUBS_PoC.ps1
@@ -16,7 +16,6 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host -ForegroundColor Green "Start OSDCloud"
-#Start-OSDCloud -OSLanguage de-de -OSBuild "21H1" -OSEdition Enterprise -ZTI
 Start-OSDCloud -OSLanguage en-us -OSBuild "20H2" -OSEdition Enterprise -ZTI
 
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\Autopilot.cmd"
@@ -26,11 +25,20 @@ Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 REM Start PowerShell -NoL -W Mi
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 REM $Computername = 'PoC-' + ((Get-CimInstance -ClassName Win32_BIOS).SerialNumber).Trim()
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows -RemoveAppx Xbox,Zune
 Start /Wait PowerShell -NoL -C Start-AutopilotOOBE -Title 'EDUBS PoC Autopilot Register' -Hidden AssignedUser,AssignedComputerName -AddToGroup sg-Autopilot -Grouptag sg-Autopilot -Assign -PostAction SysprepReboot
 '@
+$AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\PostOOBE.cmd' -Encoding ascii -Force
 
-$AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\Autopilot.cmd' -Encoding ascii -Force
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\PostOOBE.cmd"
+$PostOOBECMD = @'
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+REM Start PowerShell -NoL -W Mi
+Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows -RemoveAppx Xbox,Zune
+'@
+
+$PostOOBECMD | Out-File -FilePath 'C:\Windows\System32\PostOOBE.cmd' -Encoding ascii -Force
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
