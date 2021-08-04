3a4b89825b99806a8d6ccc2229a2144b977417a6
AkosBakos <bakosa@gmail.com>
2021-08-04 08:20:39 +0200

    Update GYMKG.ps1

--- ScriptPad/GYMKG.ps1
+++ ScriptPad/GYMKG.ps1
@@ -1,28 +1,33 @@
 
 #================================================
-#   [OS] Starting OSDCloud ZTI
+#   [PreOS] Update Module
 #================================================
-
-#Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
 
-#Make sure I have the latest OSD Content
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force   
 
-#Start OSDCloud ZTI
-Write-Host -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "20H2" -OSEdition Enterprise -ZTI
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSBuild = "20H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "de-de"
+    ZTI = $true
+}
+Start-OSDCloud @Params
 
 #================================================
 #  [PostOS] AutopilotOOBE Configuration Staging
 #================================================
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
 $AutopilotOOBEJson = @'
 {
     "Assign":  {
@@ -52,6 +57,9 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 }
 $AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
 
+#================================================
+#  [PostOS] AutopilotOOBE CMD Command Line
+#================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\Autopilot.cmd"
 $AutopilotCMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
@@ -62,21 +70,52 @@ Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
 $AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\Autopilot.cmd' -Encoding ascii -Force
 
 #================================================
-#  [PostOS] AutopilotOOBE Configuration Staging
+#  [PostOS] OOBEDeploy Configuration
+#================================================
+Write-Host -ForegroundColor Green "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
+$OOBEDeployJson = @'
+{
+    "Autopilot":  {
+                      "IsPresent":  false
+                  },
+    "RemoveAppx":  [
+                       "CommunicationsApps",
+                       "OfficeHub",
+                       "People",
+                       "Skype",
+                       "Solitaire",
+                       "Xbox",
+                       "ZuneMusic",
+                       "ZuneVideo"
+                   ],
+    "UpdateDrivers":  {
+                          "IsPresent":  true
+                      },
+    "UpdateWindows":  {
+                          "IsPresent":  true
+                      }
+}
+'@
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+#================================================
+#  [PostOS] OOBEDeploy CMD Command Line
 #================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\PostOOBE.cmd"
 $PostOOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-REM Start PowerShell -NoL -W Mi
+Start PowerShell -NoL -W Mi
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows -RemoveAppx Xbox,Zune
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 
 $PostOOBECMD | Out-File -FilePath 'C:\Windows\System32\PostOOBE.cmd' -Encoding ascii -Force
 
-#Restart from WinPE
+#=======================================================================
+#   PostOS: Restart-Computer
+#=======================================================================
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
 wpeutil reboot
