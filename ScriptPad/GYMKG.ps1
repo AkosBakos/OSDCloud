2d64e6ae4c1d5ceff4a87173b488fe759e6485b8
AkosBakos <bakosa@gmail.com>
2021-08-04 15:13:14 +0200

    -

--- ScriptPad/GYMKG.ps1
+++ ScriptPad/GYMKG.ps1
@@ -49,33 +49,6 @@ $AuditUnattendXml = @'
             </RunSynchronous>
         </component>
     </settings>
-</unattend>
-    <settings pass="auditUser">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <RunSynchronous>
-            
-            <RunSynchronousCommand wcm:action="add">
-            <Order>1</Order>
-            <Description>Setting PowerShell ExecutionPolicy</Description>
-            <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy RemoteSigned -Force"</Path>
-            </RunSynchronousCommand>
-
-            <RunSynchronousCommand wcm:action="add">
-            <Order>2</Order>
-            <Description>Update OSD Module</Description>
-            <Path>PowerShell -Command "Install-Module OSD -Force"</Path>
-            </RunSynchronousCommand>
-
-            <RunSynchronousCommand wcm:action="add">
-            <Order>3</Order>
-            <Description>OOBEDeploy</Description>
-            <Path>PowerShell -Command "Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows"</Path>
-            </RunSynchronousCommand>
-
-            </RunSynchronous>
-        </component>
-    </settings>
-</unattend>
 '@
 #================================================
 #   Set Unattend.xml
