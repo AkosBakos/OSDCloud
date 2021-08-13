4c31b4ff2f18358fb8b17d8f6c6941c22813a688
AkosBakos <44706641+AkosBakos@users.noreply.github.com>
2021-08-13 14:22:11 +0200

    Update Set-KeyboardLanguage.ps1

--- Set-KeyboardLanguage.ps1
+++ Set-KeyboardLanguage.ps1
@@ -1,10 +1,11 @@
-Write-Host -ForegroundColor Green "Set keyboard language for de-CH"
+Write-Host -ForegroundColor Green "Add de-CH keyboard layout"
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("de-CH")
 Set-WinUserLanguageList $LanguageList -Force
 
 Sleep 5
 
+Write-Host -ForegroundColor Green "Remove de-DE keyboard layout"
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'de-DE'))
 Set-WinUserLanguageList $LanguageList -Force
