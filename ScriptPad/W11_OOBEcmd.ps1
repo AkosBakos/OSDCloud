a593e4e20624479730e265b422433d3519f4bf72
AkosBakos <bakosa@gmail.com>
2022-10-30 16:18:44 +0100

    Rename scripts

--- ScriptPad/W11_OOBEcmd.ps1
+++ ScriptPad/W11_OOBEcmd.ps1
@@ -133,6 +133,7 @@ Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://check-autopilotprereq.
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://start-autopilotoobe.osdcloud.ch
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://tpm.osdcloud.ch
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Lenovo_BIOS_Settings.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://cleanup.osdcloud.ch
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
