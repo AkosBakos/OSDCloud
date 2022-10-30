1b771e82fd81f77f47de8cc75e62099e02ee9ae0
AkosBakos <44706641+AkosBakos@users.noreply.github.com>
2022-10-30 15:15:33 +0100

    Update GitHubTestScript.ps1

--- GitHubTestScript.ps1
+++ GitHubTestScript.ps1
@@ -1,4 +1,4 @@
-function Test-PutThisToAzureKeyVault
+function Test-GitHubScript
 {
     [CmdletBinding()]
     param()
@@ -7,5 +7,5 @@ function Test-PutThisToAzureKeyVault
 if (Get-Command Test-PutThisToAzureKeyVault)
 {
   Write-Host 'Test-PutThisToAzureKeyVault function is added to this PowerShell session' -ForegroundColor Green
-  Test-PutThisToAzureKeyVault -Verbose
+  Test-GitHubScript -Verbose
 }
